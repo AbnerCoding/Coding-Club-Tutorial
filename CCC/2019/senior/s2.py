10ae2a846dc79d4e4e3a0af0a224d8283118fef8
AbnerCoding <125511208+AbnerCoding@users.noreply.github.com>
2024-02-01 20:29:11 -0330

    Update s2.py

--- CCC/2019/senior/s2.py
+++ CCC/2019/senior/s2.py
@@ -1,42 +1,26 @@
 def isPrime(num):
-    yPrime = False
-    if num == 1:
-        return yPrime
-    if num > 1:
-        for iter in range(2, num):
-            if num % iter == 0:
-                yPrime = True
-                break
-
-        if yPrime:
-            return 0
-        else:
-            return 1
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 lines = int(input())
 num_list = []
 ans_list = []
-
 
 for line in range(lines):
     num_list.append(int(input()))
 
 for num in num_list:
     temp = []
-    for i in range(0, num*2):
-        for j in range(0, num*2):
-            if isPrime(i) and isPrime(j):
-                if (i + j)/2 == num:
-                    temp.append((i, j))
-                    break
-                else:
-                    continue
-        if len(temp) != 0:
-
-            ans_list.append(temp[0])
+    for i in range(2, num * 2):
+        if isPrime(i) and isPrime(num * 2 - i):
+            temp.append((i, num * 2 - i))
             break
-        else:
-            continue
+
+    ans_list.append(temp[0])
 
 for i in ans_list:
-    print(str(i[0]) + ' ' + str(i[1]))
+    print(f"{i[0]} {i[1]}")
